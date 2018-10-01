f81de28d1b8ce209c068a5575dd0f010f7e9abae
Steven Phan <ptviet@live.com>
2018-10-01 14:54:49 +1000

    Initial Commit

---
+++ FirebaseSocialLogin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseSocialLogin
+//
+//  Created by Steven Phan on 1/10/18.
+//  Copyright © 2018 SP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
